c47338a9a9323fd64a8969f2f8cc94231f12b9b1
Kevin Imvoo <Kevin Imvoo>
2016-04-09 11:16:12 +1000

    Add BG and start work on character.

--- js/game.js
+++ js/game.js
@@ -1,36 +1,54 @@
 var screenWidth = 800;
 var screenHeight = 600;
 
-var game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, '', { preload: preload, create:create, update:update });
+var game = new Phaser.Game(screenWidth, screenHeight, Phaser.CANVAS, '', { preload: preload, create:create, update:update });
 
 var t;
+var character;
+var keyInput;
 
 function preload() {
-    
+	game.load.image("char", "../img/circle.png");
+	game.load.image("background", "../img/background.png");
 }
 
 function create() {
-    game.stage.disableVisibilityChange = true;
-    
-    var text = "Hello world!";
-    var style = { font: "60px   Arial", fill:"White" };
-    
-    t = game.add.text(game.world.centerX, screenHeight/2, text, style);
-    t.moveLeft = true;
+	game.stage.disableVisibilityChange = true;
+	game.stage.backgroundColor = "#ffffff";
+	game.add.image(0, 0, "background");
+		
+	var text = "Hello world!";
+	var style = { font: "60px   Arial", fill:"Black" };
+	
+	t = game.add.text(game.world.centerX, screenHeight/2, text, style);
+	t.moveLeft = true;
+	
+	character = game.add.sprite(200, 200, "char");
+	character.body.fixedRotation = true;
+	
+	keyInput = game.input.keyboard.createCursorKeys();
+	
 }
 
 function update() {
-    if (t.x > 0 && t.moveLeft) {
-        t.x -= 5
-    }
-    else {
-        t.moveLeft = false;
-    }
-    
-    if (t.moveLeft == false) {
-        t.x += 5
-        if (t.x + t.width > 800) {
-            t.moveLeft = true;
-        }
-    }
+	MoveText();
+	
+	
+	
+}
+
+function MoveText() {
+	if (t.x > 0 && t.moveLeft) {
+		t.x -= 5
+	}
+	else {
+		t.moveLeft = false;
+	}
+	
+	if (t.moveLeft == false) {
+		t.x += 5
+		if (t.x + t.width > 800) {
+			t.moveLeft = true;
+		}
+	}    
 }
