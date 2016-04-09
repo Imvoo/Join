1ba294bbdf1e0e3b714f0848f17b86459b2dd3e3
Kevin Imvoo <Kevin Imvoo>
2016-04-09 15:38:17 +1000

    Forgot the port oops.

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res) {
     res.sendfile('index.html'); 
 });
+
+http.listen(2345, function() {
+   console.log("Listening on :2345."); 
+});
